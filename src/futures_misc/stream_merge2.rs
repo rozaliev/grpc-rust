60de66f79a1f3726d1e7b41b196e7c89229420d1
Evgeny Rozaliev <rozaliev@gmail.com>
2016-11-24 19:55:28 +0300

    Make it compile

--- src/futures_misc/stream_merge2.rs
+++ src/futures_misc/stream_merge2.rs
@@ -1,5 +1,4 @@
 use futures::*;
-use futures::stream::Stream;
 
 pub struct StreamMerge2<S1, S2: Stream> {
     stream1: stream::Fuse<S1>,
@@ -9,7 +8,8 @@ pub struct StreamMerge2<S1, S2: Stream> {
 }
 
 pub fn stream_merge2<S1, S2>(stream1: S1, stream2: S2) -> StreamMerge2<S1, S2>
-    where S1: Stream, S2: Stream<Error = S1::Error>
+    where S1: Stream,
+          S2: Stream<Error = S1::Error>
 {
     StreamMerge2 {
         stream1: stream1.fuse(),
@@ -28,28 +28,33 @@ pub enum Merged2Item<I1, I2> {
     Second(I2),
 }
 
-fn poll2<S1, S2>(s1: &mut S1, s2: &mut S2)
-    -> Poll<Option<Merged2Item<S1::Item, S2::Item>>, S1::Error>
-        where
-            S1 : Stream,
-            S2 : Stream<Error = S1::Error>,
+fn poll2<S1, S2>(s1: &mut S1,
+                 s2: &mut S2)
+                 -> Poll<Option<Merged2Item<S1::Item, S2::Item>>, S1::Error>
+    where S1: Stream,
+          S2: Stream<Error = S1::Error>
 {
     match s1.poll() {
         Err(e) => Err(e),
-        Ok(Async::NotReady) => match try_ready!(s2.poll()) {
-            Some(item2) => Ok(Async::Ready(Some(Merged2Item::Second(item2)))),
-            None => Ok(Async::NotReady),
-        },
+        Ok(Async::NotReady) => {
+            match try_ready!(s2.poll()) {
+                Some(item2) => Ok(Async::Ready(Some(Merged2Item::Second(item2)))),
+                None => Ok(Async::NotReady),
+            }
+        }
         Ok(Async::Ready(Some(item1))) => Ok(Async::Ready(Some(Merged2Item::First(item1)))),
-        Ok(Async::Ready(None)) => match try_ready!(s2.poll()) {
-            Some(item2) => Ok(Async::Ready(Some(Merged2Item::Second(item2)))),
-            None => Ok(Async::Ready(None)),
-        },
+        Ok(Async::Ready(None)) => {
+            match try_ready!(s2.poll()) {
+                Some(item2) => Ok(Async::Ready(Some(Merged2Item::Second(item2)))),
+                None => Ok(Async::Ready(None)),
+            }
+        }
     }
 }
 
 impl<S1, S2> Stream for StreamMerge2<S1, S2>
-    where S1: Stream, S2: Stream<Error = S1::Error>
+    where S1: Stream,
+          S2: Stream<Error = S1::Error>
 {
     type Item = Merged2Item<S1::Item, S2::Item>;
     type Error = S1::Error;
